33807692746ff344ed3ca92ccf016fadf6de54e5
nilesh <deorenilesh@kbtcoe.org>
2022-10-19 18:56:10 +0530

    colorChange js lab

---
+++ script.js
@@ -0,0 +1,57 @@
+console.log("Hello my name is Computer with George babbage")
+
+
+function one() {
+    alert("Hello this is the alert message")
+}
+document.getElementById("1").addEventListener("click", one)
+
+
+function colorChange() {
+    let b = prompt("Jaldi se color change kar, panvel nikalna hai (golmal)")
+    
+    document.body.style.background = `${b}`
+}
+
+document.getElementById("c").addEventListener("click",colorChange)
+
+
+//Code of textarea 
+// let ta = document.getElementById("t");
+// console.log(ta);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
